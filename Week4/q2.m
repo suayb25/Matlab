3fdbadd53aa9d366d826c353006798fbe33ac20d
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2021-03-16 15:21:15 +0300

    Add files via upload

---
+++ Week4/q2.m
@@ -0,0 +1,23 @@
+number = input("Enter a number: ");
+switch number
+    case 1
+        disp("|");
+    case 2
+        disp("||");
+    case 3
+        disp("|||");
+    case 4
+        disp("|V");
+    case 5
+        disp("V");
+    case 6
+        disp("V|");
+    case 7
+        disp("V||");
+    case 8
+        disp("V|||");
+    case 9
+        disp("|X");
+    otherwise
+        disp("Enter a number between 1 and 10");     
+end
