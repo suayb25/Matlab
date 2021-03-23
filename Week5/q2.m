739301c5f90c14f248f603a5fdd26c05c6d1ba3e
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2021-03-23 15:23:15 +0300

    Add files via upload

---
+++ Week5/q2.m
@@ -0,0 +1,14 @@
+x = input("Enter x:");
+y = input("Enter y:");
+product = 1;
+for i = 1:y
+    product = product * x;
+end
+disp(x + " power to " + y + " : " + product);
+% x = input("Enter x:");
+% y = input("Enter y:");
+% product = x;
+% for i = 1:y-1
+%     product = product * x;
+% end
+% disp(x + " power to " + y + " : " + product);
