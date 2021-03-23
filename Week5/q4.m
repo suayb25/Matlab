6bca213cd2ddc23841241de7611a52777cf06079
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2021-03-23 15:46:49 +0300

    Add files via upload

---
+++ Week5/q4.m
@@ -0,0 +1,12 @@
+number = input("Enter a number: ");
+total = 1;
+for i = 2:number-1 %number/2
+    if mod(number,i) == 0 %rem(number,i) == 0
+        total = total + i;
+    end
+end
+if number == total
+    disp("Perfect")
+else
+    disp("Not perfect");
+end
