399e75f8da7eaf80b34a1fe43fe803809619691d
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2021-05-04 15:37:05 +0300

    Add files via upload

---
+++ Week11/writeOnesDigit.m
@@ -0,0 +1,22 @@
+function displayed = writeOnesDigit(number)
+    switch number
+        case 1
+            displayed = "one";
+        case 2
+            displayed = "two";
+        case 3 
+            displayed = "three";
+        case 4
+            displayed = "four";
+        case 5
+            displayed = "five";
+        case 6
+            displayed = "six";
+        case 7
+            displayed = "seven";
+        case 8
+            displayed = "eight";
+        case 9
+            displayed = "nine";
+    end
+end
