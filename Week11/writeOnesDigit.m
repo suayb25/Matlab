cde4e5d085fa03b54fb3d175cfaec676bf8113fc
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2022-05-09 09:20:17 +0300

    Add files via upload

--- Week11/writeOnesDigit.m
+++ Week11/writeOnesDigit.m
@@ -1,10 +1,13 @@
 function displayed = writeOnesDigit(number)
+    number = mod(number, 10);
     switch number
+        case 0
+            displayed = "zero";
         case 1
             displayed = "one";
         case 2
             displayed = "two";
-        case 3 
+        case 3
             displayed = "three";
         case 4
             displayed = "four";
@@ -18,5 +21,7 @@ function displayed = writeOnesDigit(number)
             displayed = "eight";
         case 9
             displayed = "nine";
+        otherwise
+            displayed = "None";
     end
 end
