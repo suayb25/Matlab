92a2800dfd954212c05089fffc2a22990764e9e9
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2022-05-16 08:29:34 +0300

    Add files via upload

--- Week12/q2.m
+++ Week12/q2.m
@@ -1,4 +1,7 @@
 days = ["Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday"];
-day = floor(input("Enter day of week: ")); %as int
-day = mod(day,8)+1;
+day = input("Enter day of week: "); %as int
+while day>7 || day<1
+    disp("Please enter a number from 1 to 7!");
+    day = input("Enter day of week: ");
+end
 disp(days(day));
